eb4130da4794e012923d53f7868d484e0e0c8644
Dhanunjaysaikiran <dhanunjay2694@gmail.com>
2021-12-09 12:58:36 +0530

    upadte

---
+++ bookpod/index.js
@@ -0,0 +1,10 @@
+"use strict;"
+
+ function myFunction() {
+    var x = document.getElementById("oglmn");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
