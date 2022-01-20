d4dd0be2951fa57fd610ecfb068764e4e977e965
Dhanunjaysaikiran <dhanunjay2694@gmail.com>
2022-01-20 17:00:47 +0530

    update

---
+++ insight/scripts/script.js
@@ -0,0 +1,16 @@
+
+function myFunction() {
+    var x = document.getElementById("dp");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+//   Click on the coconut to see the javascript work
+
+function message (){
+    alert('Click on the Coconut, to see the javascript work! ')
+}
+message();
