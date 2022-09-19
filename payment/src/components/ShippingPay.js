df4a959f35ec106b4303619af2588bbd2b3a12ed
Dhanunjay Sai Kiran <dhanunjay2694@gmail.com>
2022-09-19 20:26:21 +0530

    progress

--- payment/src/components/ShippingPay.js
+++ payment/src/components/ShippingPay.js
@@ -1,12 +1,25 @@
 import React from "react";
-
+import Truck from "./logos/TruckBig.svg";
+import Cart from "./logos/Cart.svg";
 function ShippingPay() {
   return (
-    <div className="full pad">
+    <div className="full pad botm">
       Shipping and payment
-      <div className="topgap">
-        <button className="green xp">Login</button>
-        <button className="gap xp">Signup</button>
+      <div className="topgap flex btw">
+        <div>
+          {" "}
+          <button className="green xp">Login</button>
+          <button className="gap xp">Signup</button>
+        </div>
+        <div className="log flex">
+          <div className="lop">
+            <img src={Cart} alt="" />
+          </div>
+          <div className="poly">
+            {" "}
+            <img src={Truck} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
